280d7cd5dd4bb6c30c5db48b77438e01149ef13b
AryaFoxs <aryafoxs@gmail.com>
2026-02-25 15:04:30 +0800

    Tambah google analytics 25 Feb 2025 15.04

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { GoogleAnalytics } from '@next/third-parties/google';
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -54,6 +55,7 @@ export default function RootLayout({
       <body className="antialiased">
         {children}
       </body>
+      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID || ""} />
     </html>
   );
 }
